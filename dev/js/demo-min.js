9423ed5d2a45bca42996d5530912c17d4e5139d3
c8flores <63384523+c8flores@users.noreply.github.com>
2020-12-05 18:07:08 -0500

    setting up

--- dev/js/demo-min.js
+++ dev/js/demo-min.js
@@ -1,1 +1,1 @@
-import{gsap}from"gsap";gsap.timeline().from("Path-4-Copy",{duration:1,opacity:0}),console.log(gsap);
+import{gsap}from"gsap";console.log(gsap);
--- dev/js/demo.js
+++ dev/js/demo.js
@@ -1,6 +1,4 @@
 import {gsap} from "gsap";
 
-gsap.timeline()
-    .from("Path-4-Copy", {duration:1, opacity:0})
 
 console.log(gsap);
