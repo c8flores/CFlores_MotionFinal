993b80249649ca1a3033730f0ab8a1c38960f755
c8flores <63384523+c8flores@users.noreply.github.com>
2020-12-05 17:46:42 -0500

    starting

---
+++ dev/js/demo-min.js
@@ -0,0 +1,1 @@
+import{gsap}from"gsap";gsap.timeline().from("Path-4-Copy",{duration:1,opacity:0}),console.log(gsap);
--- dev/js/demo.js
+++ dev/js/demo.js
@@ -1,4 +1,6 @@
 import {gsap} from "gsap";
 
+gsap.timeline()
+    .from("Path-4-Copy", {duration:1, opacity:0})
 
 console.log(gsap);
