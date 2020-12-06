3c2f2e87628b5581bb401fdb9aaa102cd932a37b
c8flores <63384523+c8flores@users.noreply.github.com>
2020-12-06 14:14:04 -0500

    updates

--- deploy/js/scripts-min.js
+++ deploy/js/scripts-min.js
@@ -9742,7 +9742,7 @@
   const logoTL = gsapWithCSS.timeline();
 
   function logoAnimation(){
-    logoTL.from("#big-mustang-logo", {opacity:0, duration:4, yPercent:50, alpha:1})
+    logoTL.from("#big-mustang-logo", {alpha:1, duration:4, yPercent:50})
           .from("#mustang-logo", {opacity:0, duration:5});
       
       return logoTL;
--- dev/js/mustang-logo/mustang-logo.js
+++ dev/js/mustang-logo/mustang-logo.js
@@ -3,7 +3,7 @@ import {gsap} from "gsap"
 const logoTL = gsap.timeline();
 
 export function logoAnimation(){
-  logoTL.from("#big-mustang-logo", {opacity:0, duration:4, yPercent:50, alpha:1})
+  logoTL.from("#big-mustang-logo", {alpha:1, duration:4, yPercent:50})
         .from("#mustang-logo", {opacity:0, duration:5})
     
     return logoTL;
